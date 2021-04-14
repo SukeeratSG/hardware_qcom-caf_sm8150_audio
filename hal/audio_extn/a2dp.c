LineageOS/android_hardware_qcom_audio

a2dp: Add a null check before iterating over a2dp.adev->usecase_list
Prevents audioservice from crashing when trying to switch an incoming phone call
to a bluetooth headset

Change-Id: I130f2fe5f1f15fa61ea3075c4f32b978eef609b3

hal/audio_extn/a2dp.c
@@ -2792,13 +2792,15 @@ int a2dp_set_parameters(struct str_parms *parms, bool *reconfig)
                 a2dp.a2dp_source_suspended = true;
                 if (a2dp.bt_state_source == A2DP_STATE_DISCONNECTED)
                     goto param_handled;
-                list_for_each(node, &a2dp.adev->usecase_list) {
-                    uc_info = node_to_item(node, struct audio_usecase, list);
-                    if (uc_info->stream.out && uc_info->type == PCM_PLAYBACK &&
-                         (uc_info->stream.out->devices & AUDIO_DEVICE_OUT_ALL_A2DP)) {
-                        pthread_mutex_unlock(&a2dp.adev->lock);
-                        fp_check_a2dp_restore(a2dp.adev, uc_info->stream.out, false);
-                        pthread_mutex_lock(&a2dp.adev->lock);
+                if (a2dp.adev->usecase_list.next != NULL) {
+                    list_for_each(node, &a2dp.adev->usecase_list) {
+                        uc_info = node_to_item(node, struct audio_usecase, list);
+                        if (uc_info != NULL && uc_info->type == PCM_PLAYBACK && uc_info->stream.out &&
+                             (uc_info->stream.out->devices & AUDIO_DEVICE_OUT_ALL_A2DP)) {
+                            pthread_mutex_unlock(&a2dp.adev->lock);
+                            fp_check_a2dp_restore(a2dp.adev, uc_info->stream.out, false);
+                            pthread_mutex_lock(&a2dp.adev->lock);
+                        }
                     }
                 }
                 if (!a2dp.swb_configured)
@@ -2833,13 +2835,15 @@ int a2dp_set_parameters(struct str_parms *parms, bool *reconfig)
                         }
                     }
                 }
-                list_for_each(node, &a2dp.adev->usecase_list) {
-                    uc_info = node_to_item(node, struct audio_usecase, list);
-                    if (uc_info->stream.out && uc_info->type == PCM_PLAYBACK &&
-                         (uc_info->stream.out->devices & AUDIO_DEVICE_OUT_ALL_A2DP)) {
-                        pthread_mutex_unlock(&a2dp.adev->lock);
-                        fp_check_a2dp_restore(a2dp.adev, uc_info->stream.out, true);
-                        pthread_mutex_lock(&a2dp.adev->lock);
+                if (a2dp.adev->usecase_list.next != NULL) {
+                    list_for_each(node, &a2dp.adev->usecase_list) {
+                        uc_info = node_to_item(node, struct audio_usecase, list);
+                        if (uc_info->stream.out && uc_info->type == PCM_PLAYBACK &&
+                             (uc_info->stream.out->devices & AUDIO_DEVICE_OUT_ALL_A2DP)) {
+                            pthread_mutex_unlock(&a2dp.adev->lock);
+                            fp_check_a2dp_restore(a2dp.adev, uc_info->stream.out, true);
+                            pthread_mutex_lock(&a2dp.adev->lock);
+                        }
                     }
                 }
             }

SukeeratSG/hardware_qcom-caf_sm8150_audio

hal/audio_extn/a2dp.c
@@ -2884,13 +2884,15 @@ int a2dp_set_parameters(struct str_parms *parms, bool *reconfig)
             a2dp.a2dp_source_suspended = true;
             if (a2dp.bt_state_source == A2DP_STATE_DISCONNECTED)
                 goto param_handled;
-            list_for_each(node, &a2dp.adev->usecase_list) {
-                uc_info = node_to_item(node, struct audio_usecase, list);
-                if (uc_info->type == PCM_PLAYBACK &&
-                    (uc_info->out_snd_device == SND_DEVICE_OUT_BT_A2DP ||
-                     uc_info->out_snd_device == SND_DEVICE_OUT_SPEAKER_AND_BT_A2DP ||
-                     uc_info->out_snd_device == SND_DEVICE_OUT_SPEAKER_SAFE_AND_BT_A2DP)) {
-                    fp_check_a2dp_restore_l(a2dp.adev, uc_info->stream.out, false);
+            if (a2dp.adev->usecase_list.next != NULL) {
+                list_for_each(node, &a2dp.adev->usecase_list) {
+                    uc_info = node_to_item(node, struct audio_usecase, list);
+                    if (uc_info->type == PCM_PLAYBACK &&
+                        (uc_info->out_snd_device == SND_DEVICE_OUT_BT_A2DP ||
+                         uc_info->out_snd_device == SND_DEVICE_OUT_SPEAKER_AND_BT_A2DP ||
+                         uc_info->out_snd_device == SND_DEVICE_OUT_SPEAKER_SAFE_AND_BT_A2DP)) {
+                        fp_check_a2dp_restore_l(a2dp.adev, uc_info->stream.out, false);
+                    }
                 }
             }
             if (!a2dp.swb_configured)
@@ -2925,11 +2927,13 @@ int a2dp_set_parameters(struct str_parms *parms, bool *reconfig)
                     }
                 }
             }
-            list_for_each(node, &a2dp.adev->usecase_list) {
-                uc_info = node_to_item(node, struct audio_usecase, list);
-                if (uc_info->stream.out && uc_info->type == PCM_PLAYBACK &&
-                    is_a2dp_out_device_type(&uc_info->stream.out->device_list)) {
-                    fp_check_a2dp_restore_l(a2dp.adev, uc_info->stream.out, true);
+            if (a2dp.adev->usecase_list.next != NULL) {
+                list_for_each(node, &a2dp.adev->usecase_list) {
+                    uc_info = node_to_item(node, struct audio_usecase, list);
+                    if (uc_info->stream.out && uc_info->type == PCM_PLAYBACK &&
+                        is_a2dp_out_device_type(&uc_info->stream.out->device_list)) {
+                        fp_check_a2dp_restore_l(a2dp.adev, uc_info->stream.out, true);
+                    }
                 }
             }
         }